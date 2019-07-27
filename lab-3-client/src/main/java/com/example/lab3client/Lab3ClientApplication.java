afb532360dc33ee9bcbb6cdf2ebb5a554a7a2e41
mbronshteyn <michael.bronshteyn@gmail.com>
2019-07-27 16:59:06 -0700

    init

---
+++ lab-3-client/src/main/java/com/example/lab3client/Lab3ClientApplication.java
@@ -0,0 +1,14 @@
+package com.example.lab3client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3ClientApplication.class, args);
+	}
+
+}
---
+++ lab-3-server/src/main/java/com/example/lab3server/Lab3ServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.lab3server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Lab3ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3ServerApplication.class, args);
+	}
+
+}
