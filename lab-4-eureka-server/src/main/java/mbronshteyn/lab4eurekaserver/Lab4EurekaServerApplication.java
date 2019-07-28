1b2312edbe039465e2114933e1a71c0dd3af1e96
mbronshteyn <michael.bronshteyn@gmail.com>
2019-07-27 18:15:04 -0700

    init

---
+++ lab-4-eureka-server/src/main/java/mbronshteyn/lab4eurekaserver/Lab4EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package mbronshteyn.lab4eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Lab4EurekaServerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(Lab4EurekaServerApplication.class, args);
+  }
+}
