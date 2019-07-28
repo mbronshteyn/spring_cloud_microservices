a0869e9aa68750277c27c327d6b5c282d8f018bb
mbronshteyn <michael.bronshteyn@gmail.com>
2019-07-28 13:55:49 -0700

    add lab-4-sentence

---
+++ lab-4-sentence/src/main/java/mbronshteyn/lab4sentence/controller/SentenceController.java
@@ -0,0 +1,43 @@
+package mbronshteyn.lab4sentence.controller;
+
+import com.netflix.discovery.shared.Application;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class SentenceController {
+
+  @Autowired
+  public DiscoveryClient client;
+
+  @GetMapping("/sentence")
+  public @ResponseBody
+  String getSentence() {
+    return
+      getWord("LAB-4-SUBJECT") + " "
+        + getWord("LAB-4-VERB") + " "
+        + getWord("LAB-4-ARTICLE") + " "
+        + getWord("LAB-4-ADJECTIVE") + " "
+        + getWord("LAB-4-NOUN") + "."
+      ;
+  }
+
+  public String getWord(String service) {
+    List<ServiceInstance> list = client.getInstances(service);
+    if (list != null && list.size() > 0 ) {
+      URI uri = list.get(0).getUri();
+      if (uri !=null ) {
+        return (new RestTemplate()).getForObject(uri,String.class);
+      }
+    }
+    return null;
+  }
+}
