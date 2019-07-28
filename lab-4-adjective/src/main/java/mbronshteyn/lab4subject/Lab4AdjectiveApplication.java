84e50d391189030b2031f3f9a7f2938f75f1f4d0
mbronshteyn <michael.bronshteyn@gmail.com>
2019-07-28 12:50:28 -0700

    add lab-4-adjective

---
+++ lab-4-adjective/src/main/java/mbronshteyn/lab4subject/Lab4AdjectiveApplication.java
@@ -0,0 +1,15 @@
+package mbronshteyn.lab4subject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class Lab4AdjectiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4AdjectiveApplication.class, args);
+	}
+
+}
