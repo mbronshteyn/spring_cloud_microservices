5b3e7c09a82a5ccd099c4a7fb6bb32360aeb51c9
mbronshteyn <michael.bronshteyn@gmail.com>
2019-08-01 11:10:52 -0700

    code changes

--- lab-4-sentence/src/main/java/mbronshteyn/lab4sentence/Lab4SentenceApplication.java
+++ lab-4-sentence/src/main/java/mbronshteyn/lab4sentence/Lab4SentenceApplication.java
@@ -1,18 +1,23 @@
 package mbronshteyn.lab4sentence;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableFeignClients
+@EnableEurekaClient
 public class Lab4SentenceApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lab4SentenceApplication.class, args);
--- lab-4-sentence/src/main/java/mbronshteyn/lab4sentence/controller/SentenceController.java
+++ lab-4-sentence/src/main/java/mbronshteyn/lab4sentence/controller/SentenceController.java
@@ -1,13 +1,19 @@
 package mbronshteyn.lab4sentence.controller;
 
 import mbronshteyn.lab4sentence.repo.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SentenceController {
+
+  @Value("${server.port}")
+  Long port;
 
   @Autowired
   SubjectRepo subjectRepo;
@@ -24,8 +30,13 @@ public class SentenceController {
   @Autowired
   NounRepo nounRepo;
 
+  Logger logger = LoggerFactory.getLogger(this.getClass());
+
   @GetMapping("/sentence")
   public @ResponseBody String getSentence() {
+
+    logger.info( "Logger class instance id: " + logger.getClass().toString() );
+
     return
         subjectRepo.getItem() + " "
         + verbRepo.getItem() + " "
