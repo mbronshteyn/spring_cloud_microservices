481d8b5b0f9deb77b4ea2974cb91d91c84a7895c
mbronshteyn <michael.bronshteyn@gmail.com>
2019-08-01 08:34:45 -0700

    remove unneeded code

--- lab-4-sentence/src/main/java/mbronshteyn/lab4sentence/Lab4SentenceApplication.java
+++ lab-4-sentence/src/main/java/mbronshteyn/lab4sentence/Lab4SentenceApplication.java
@@ -14,17 +14,9 @@ import org.springframework.web.client.RestTemplate;
 @EnableFeignClients
 public class Lab4SentenceApplication {
 
-	@Bean
-	@Qualifier( value= "LoadBalanced" )
-	@LoadBalanced
-	RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(Lab4SentenceApplication.class, args);
 	}
-
 
 }
 
