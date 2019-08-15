4f8208eb58f37b9d135251785a3516567b44e11e
mbronshteyn <michael.bronshteyn@gmail.com>
2019-08-15 09:01:57 -0700

    add error handling

--- lab-4-sentence/src/main/java/mbronshteyn/lab4sentence/controller/SentenceController.java
+++ lab-4-sentence/src/main/java/mbronshteyn/lab4sentence/controller/SentenceController.java
@@ -1,13 +1,17 @@
 package mbronshteyn.lab4sentence.controller;
 
+import mbronshteyn.lab4sentence.model.CustomException;
 import mbronshteyn.lab4sentence.repo.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class SentenceController {
@@ -33,12 +37,27 @@ public class SentenceController {
   Long port;
 
   @GetMapping( "/port" )
-  public @ResponseBody Long getPort(){
+  public @ResponseBody Long getPort( @RequestHeader Map<String, String> headers){
+    logger.info( "=============================================" );
+    headers.forEach( ( key, value ) ->  logger.info( String.format( "Header Name: %s, Value: %s", key, value )));
+    logger.info( "=============================================" );
     return port;
+  }
+
+  @GetMapping( "/exception" )
+  public @ResponseBody Long getException() throws Exception {
+    throw new Exception("test");
+  }
+
+  @GetMapping( "/custom-exception" )
+  public @ResponseBody Long getCustomException() throws Exception {
+    throw new CustomException();
   }
 
   @GetMapping("/sentence")
   public @ResponseBody String getSentence() {
+
+
 
     return
         subjectRepo.getItem() + " "
